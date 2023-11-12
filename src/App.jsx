0b6f8d058833a90b05b5f575cb57283e55b74394
Kamil Pitula <pitula.k@gmail.com>
2023-11-12 18:13:17 +0100

    install and configure react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import GamePage from "./components/Pages/GamePage";
+import { useParams } from "react-router-dom";
 
 function App() {
+  const { boardId } = useParams();
   return (
     <>
       <Header />
       <main>
+        {boardId && <h3>{boardId}</h3>}
         <GamePage></GamePage>
       </main>
     </>
