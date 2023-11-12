1dc726c197d4ac956f0182bbe8e1f735fe82abda
Kamil Pitula <pitula.k@gmail.com>
2023-11-12 17:01:06 +0100

    extract game page to a separate component in preparation for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,98 +1,13 @@
-import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
-import Backdrop from "./components/UI/Backdrop/Backdrop";
-import Settings from "./components/Settings/Settings";
-import Controls from "./components/Controls/Controls";
-import Board from "./components/Board/Board";
-import Game from "./game/game";
-import { exportToLife106 } from "./export/exportLife106";
-import { downloadFile } from "./utils/fileDownload";
+import GamePage from "./components/Pages/GamePage";
 
 function App() {
-  const [settingsModalOpen, setSettingsModalOpen] = useState(false);
-  const [board, setBoard] = useState(Game.createNewBoard(60, 60));
-  const [isRunning, setRunning] = useState(false);
-
-  useEffect(() => {
-    let frameId;
-    const tick = () => {
-      if (!isRunning) return;
-      const newBoard = Game.tick(board);
-      setBoard(newBoard);
-      frameId = requestAnimationFrame(tick);
-    };
-    frameId = requestAnimationFrame(tick);
-    return () => cancelAnimationFrame(frameId);
-  }, [board, isRunning]);
-
-  const userClickedCellHandler = useCallback(
-    (positionX, positionY) => handleCellStateChanged(positionX, positionY),
-    []
-  );
-
-  function startGame() {
-    setRunning(true);
-  }
-
-  function stopGame() {
-    setRunning(false);
-  }
-
-  function closeSettings() {
-    setSettingsModalOpen(false);
-  }
-
-  function openSettings() {
-    setSettingsModalOpen(true);
-  }
-
-  function applySettingsHandler(opt) {
-    setSettingsModalOpen(false);
-    setRunning(false);
-    setBoard(Game.createNewBoard(opt.width, opt.height));
-    console.log(opt);
-  }
-
-  function resetGame() {
-    stopGame();
-    setBoard(Game.createNewBoard(60, 60));
-  }
-
-  function exportBoard() {
-    const exportedBoard = exportToLife106(board);
-    downloadFile(exportedBoard, "pattern.life");
-  }
-
-  function handleCellStateChanged(positionX, positionY) {
-    setBoard((oldBoard) =>
-      Game.changeCellState(oldBoard, positionX, positionY)
-    );
-  }
-
   return (
     <>
-      {settingsModalOpen && (
-        <Backdrop onClick={closeSettings}>
-          <Settings
-            onCancel={closeSettings}
-            onApply={applySettingsHandler}
-          ></Settings>
-        </Backdrop>
-      )}
       <Header />
       <main>
-        <section className="game">
-          <Controls
-            onSettings={openSettings}
-            onRun={startGame}
-            onStop={stopGame}
-            onRestart={resetGame}
-            onExport={exportBoard}
-            isRunning={isRunning}
-          ></Controls>
-          <Board cells={board} onCellClicked={userClickedCellHandler}></Board>
-        </section>
+        <GamePage></GamePage>
       </main>
     </>
   );
---
+++ src/components/Pages/GamePage.jsx
@@ -0,0 +1,92 @@
+import Controls from "../Controls/Controls";
+import Board from "../Board/Board";
+import Backdrop from "../UI/Backdrop/Backdrop";
+import Settings from "../Settings/Settings";
+import { useState, useCallback, useEffect } from "react";
+import Game from "../../game/game";
+import { downloadFile } from "../../utils/fileDownload";
+import { exportToLife106 } from "../../export/exportLife106";
+import './GamePage.css';
+
+export default function GamePage() {
+  const [settingsModalOpen, setSettingsModalOpen] = useState(false);
+  const [board, setBoard] = useState(Game.createNewBoard(60, 60));
+  const [isRunning, setRunning] = useState(false);
+
+  useEffect(() => {
+    let frameId;
+    const tick = () => {
+      if (!isRunning) return;
+      const newBoard = Game.tick(board);
+      setBoard(newBoard);
+      frameId = requestAnimationFrame(tick);
+    };
+    frameId = requestAnimationFrame(tick);
+    return () => cancelAnimationFrame(frameId);
+  }, [board, isRunning]);
+
+  const userClickedCellHandler = useCallback(
+    (positionX, positionY) => handleCellStateChanged(positionX, positionY),
+    []
+  );
+
+  function startGame() {
+    setRunning(true);
+  }
+
+  function stopGame() {
+    setRunning(false);
+  }
+
+  function closeSettings() {
+    setSettingsModalOpen(false);
+  }
+
+  function openSettings() {
+    setSettingsModalOpen(true);
+  }
+
+  function applySettingsHandler(opt) {
+    setSettingsModalOpen(false);
+    setRunning(false);
+    setBoard(Game.createNewBoard(opt.width, opt.height));
+    console.log(opt);
+  }
+
+  function resetGame() {
+    stopGame();
+    setBoard(Game.createNewBoard(60, 60));
+  }
+
+  function exportBoard() {
+    const exportedBoard = exportToLife106(board);
+    downloadFile(exportedBoard, "pattern.life");
+  }
+
+  function handleCellStateChanged(positionX, positionY) {
+    setBoard((oldBoard) =>
+      Game.changeCellState(oldBoard, positionX, positionY)
+    );
+  }
+  return (
+    <section className="game">
+      {settingsModalOpen && (
+        <Backdrop onClick={closeSettings}>
+          <Settings
+            onCancel={closeSettings}
+            onApply={applySettingsHandler}
+          ></Settings>
+        </Backdrop>
+      )}
+      <Controls
+        onSettings={openSettings}
+        onRun={startGame}
+        onStop={stopGame}
+        onRestart={resetGame}
+        onExport={exportBoard}
+        isRunning={isRunning}
+      ></Controls>
+      <Board cells={board} onCellClicked={userClickedCellHandler}></Board>
+    </section>
+  );
+}
