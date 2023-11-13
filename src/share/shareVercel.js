ed4dc35ce842cde8b51f3b553fa664b4aacc5d54
Kamil Pitula <pitula.k@gmail.com>
2023-11-13 20:13:26 +0100

    use vite env variables

--- src/share/shareVercel.js
+++ src/share/shareVercel.js
@@ -1,10 +1,14 @@
-import { kv } from "@vercel/kv";
+import { createClient } from "@vercel/kv";
 import { exportToLife106 } from "../export/exportLife106";
 
 const keyPrefix = "sharedBoard";
 const expireAfterSeconds = 3600;
 
 export async function saveBoard(board) {
+  const kv = createClient({
+    token: import.meta.env.VITE_KV_REST_API_TOKEN,
+    url: import.meta.env.VITE_KV_REST_API_URL,
+  });
   const boardString = exportToLife106(board);
   const uuid = crypto.randomUUID();
   await kv.set(`${keyPrefix}:${uuid}`, boardString, {
