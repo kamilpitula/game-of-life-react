73e56a43e75ed05cce4afb8c3d72839b4ec77155
Kamil Pitula <pitula.k@gmail.com>
2023-11-13 23:57:57 +0100

    build shareable url

--- src/components/Pages/ShareKeyPage/ShareKeyPage.jsx
+++ src/components/Pages/ShareKeyPage/ShareKeyPage.jsx
@@ -1,7 +1,17 @@
+import { useSearchParams } from "react-router-dom";
+
 export default function ShareKeyPage() {
+  const [searchParams] = useSearchParams();
+
+  const origin = window.location.origin;
+  const boardId = searchParams.get("boardId");
+
+  const url = `${origin}/${boardId}`;
+
   return (
     <section>
-      <h2>{"Here's your key"}</h2>
+      {/* TODO: create copy to clipboard component */}
+      <h2>{`Here's your key: ${url}`}</h2>
     </section>
   );
 }
